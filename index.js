1b0002f793bbb9c4b21dc2ddbf2725c6a3bf4a22
Filip Chalupa <chalupa.filip@gmail.com>
2023-02-22 01:59:38 +0100

    Basic functionality

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
+import chalk from 'chalk'
+import inquirer from 'inquirer'
 import { exit } from 'process'
+import simpleGit from 'simple-git'
 
-console.log('Hello there')
-console.log('This is a placeholder package. Real code comming.')
+// @TODO: configurable
 
-exit(1)
+const remoteName = 'origin'
+const branchNamePrefix = 'deploy'
+
+const git = simpleGit()
+
+// @TODO: check current working directory is git repository
+
+const remoteBranches = (await git.branch(['-r'])).all.map((branch) =>
+	branch.startsWith(`${remoteName}/`)
+		? branch.substring(remoteName.length + 1)
+		: branch,
+)
+const deployBranches = remoteBranches.filter(
+	(branch) =>
+		branch === branchNamePrefix || branch.startsWith(`${branchNamePrefix}/`),
+)
+
+if (deployBranches.length === 0) {
+	console.error(
+		chalk.red(
+			`Not a single deploy branch found in "${remoteName}" starting with ${branchNamePrefix}.`,
+		),
+	)
+	exit(1)
+}
+
+const { targetBranches } = await inquirer.prompt({
+	type: 'checkbox',
+	name: 'targetBranches',
+	message: `Which branch do you want ${chalk.magenta('HEAD')} to push to?`,
+	choices: deployBranches,
+})
+
+if (targetBranches.length === 0) {
+	console.log(chalk.yellow('No branch selected.'))
+	exit(0)
+}
+
+for (const targetBranch of targetBranches) {
+	console.log(
+		`Pushing ${chalk.magenta('HEAD')} to ${chalk.magenta(targetBranch)}…`,
+	)
+	await git.push(remoteName, `HEAD:${targetBranch}`)
+}
+
+console.log(chalk.green('Done. 🎉'))
